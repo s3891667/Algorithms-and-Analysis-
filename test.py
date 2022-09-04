bef5d18ab370aa7b117c4b005afa9035b933bfe5
s3891667 <s3891667@rmit.edu.vn>
2022-09-05 03:38:04 +1000

    add generator

--- test.py
+++ test.py
@@ -1,6 +1,24 @@
-# removing the new line characters
-with open('./sampleDataToy.txt') as f:
-    lines = [line.split() for line in f]
+import random 
+#generating input 
+# with open('./sampleDataToy.txt') as f:
+#     lines = [line.split() for line in f]
   
-for i in range(len(lines)):
-    print(lines[i][0])
+# for i in range(len(lines)):
+#     print(lines[i][0])
+    
+
+#generating data 
+file = open('./sampleData200k.txt','r').read().splitlines()
+def generatingData(size,data):
+    file = open('data' + str(size) + ".txt",'a')
+    while(size!=0):
+      file.write(random.choice(data)+ '\n')
+      size -=1
+    file.close()
+generatingData(500,file)        
+    
+    
+    
+    
+    
+    
